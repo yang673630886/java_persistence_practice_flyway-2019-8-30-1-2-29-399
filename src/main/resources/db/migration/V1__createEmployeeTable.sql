86a48bdfe1f2834ec8e1dbdf5decd0bbc358775c
yangjingsheng <673630886@qq.com>
2019-08-30 17:53:06 +0800

    添加Oracle

---
+++ src/main/resources/db/migration/V1__createEmployeeTable.sql
@@ -0,0 +1,16 @@
+CREATE TABLE employee
+(
+   id INTEGER PRIMARY KEY,
+   name VARCHAR (64) NOT NULL,
+   age int (4) NOT NULL,
+   position VARCHAR (64) NOT NULL
+);
+CREATE TABLE user
+(
+   id INTEGER PRIMARY KEY,
+   username VARCHAR (64) NOT NULL,
+   password VARCHAR (64) NOT NULL,
+   password_expired VARCHAR (64) NOT NULL,
+   role VARCHAR (64) NOT NULL,
+   createTime date
+);
--- src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE test (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(64) NOT NULL,
-  age   int(4) NOT NULL
-);
